fd555fefa37c39e495c701c50a02147c81edffff
Jonathan Clarke <jonathan@vayu.com.au>
2017-04-14 10:30:18 +0545

    Added ex_doc for testing Adding pipe function File I/O Worked with Enum and List modules Added tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,81 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+  Provides methods for creating and handling a deck of cards
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
+  Creates a full card deck and returns a list of cards
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
+
+    for suit <- suits, value <- values do
+      "#{ value } #{ suit }"
+    end    
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+
+  @doc """
+  Checks if the deck contains a specific card
+  ## Examples
+  
+      iex> deck = Cards.create_deck
+      iex> result = Cards.contains?(deck, "Ace Diamonds")
+      iex> result
+      true
+      iex> result = Cards.contains?(deck, "Pity the fool")
+      iex> result
+      false
+      
+  """  
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+
+  @doc """
+  Divides a deck into a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should
+  be in the hand
+  ## Examples
+  
+      iex> deck = Cards.create_deck
+      iex> {hand, _deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace Diamonds"]
+      
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  #Using a built in erlang method to save to a filesystem
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  #Using a built in erlang method to load from a filesystem
+  # Error handling condensed
+  # We pattern match the result of File.read(filename)
+  # if :ok then we read from that file
+  # if :error then return an error message
+  def load(filename) do
+    case File.read(filename) do
+      { :ok, binary } -> :erlang.binary_to_term(binary)
+      { :error, _reason } -> "That file does not exist"
+    end
+
+  end
+
+  def create_hand(size_of_deck) do
+    #Pipe operator; it passes the returned value and
+    #injects as the first value of the subsequent method
+    Cards.create_deck |> Cards.shuffle |> Cards.deal(size_of_deck)    
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,33 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create_deck" do
+    deck = Cards.create_deck
+    assert deck == ["Ace Diamonds", "Two Diamonds", "Three Diamonds", "Four Diamonds", "Five Diamonds", "Six Diamonds", "Seven Diamonds", "Eight Diamonds", "Nine Diamonds", "Ten Diamonds", "Jack Diamonds", "Queen Diamonds", "King Diamonds", "Ace Hearts", "Two Hearts", "Three Hearts", "Four Hearts", "Five Hearts", "Six Hearts", "Seven Hearts", "Eight Hearts", "Nine Hearts", "Ten Hearts", "Jack Hearts", "Queen Hearts", "King Hearts", "Ace Spades", "Two Spades", "Three Spades", "Four Spades", "Five Spades", "Six Spades", "Seven Spades", "Eight Spades", "Nine Spades", "Ten Spades", "Jack Spades", "Queen Spades", "King Spades", "Ace Clubs", "Two Clubs", "Three Clubs", "Four Clubs", "Five Clubs", "Six Clubs", "Seven Clubs", "Eight Clubs", "Nine Clubs", "Ten Clubs", "Jack Clubs", "Queen Clubs", "King Clubs"]
+
+  end
+
+  test "create_deck makes 52 cards" do
+    deck_length = length( Cards.create_deck )
+    assert deck_length == 52
+  end
+
+  test "shuffle should randomize the entire deck" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+    refute deck == Cards.shuffle(deck)
+  end
+
+  test "deal should split a deck into the hand size" do
+    deck = Cards.create_deck
+    {hand, deck} = Cards.deal(deck, 1)
+    assert length(hand) == 1
+    assert length(deck) == 51
+  end
+
+  test "create_hand deal a hand" do
+    {hand, deck} = Cards.create_hand(1)
+    assert length(hand) == 1
+    assert length(deck) == 51
   end
 end
